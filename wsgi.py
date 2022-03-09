f69ba2a8058b545f761bf21386d2a61b6f5b0dbf
GPortas <hey@gportas.me>
2022-03-09 10:35:23 +0000

    Changed: env config setup using env variable in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
+import os
+
 from app import create_app
 
-app = create_app(config_name='production')
+app = create_app(os.getenv('ENV_CONFIG') or 'default')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
