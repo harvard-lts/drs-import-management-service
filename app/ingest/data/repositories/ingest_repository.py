bad456d64961dd148be81e2a3fff4e4673ef6632
GPortas <hey@gportas.me>
2022-04-01 15:18:58 +0100

    Fixed: missing dummy params added to ingest repository impl

--- app/ingest/data/repositories/ingest_repository.py
+++ app/ingest/data/repositories/ingest_repository.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from app.ingest.domain.models.ingest.depositing_application import DepositingApplication
 from app.ingest.domain.models.ingest.ingest import Ingest
+from app.ingest.domain.models.ingest.ingest_status import IngestStatus
 from app.ingest.domain.repositories.ingest_repository import IIngestRepository
 
 
@@ -15,7 +16,9 @@ class IngestRepository(IIngestRepository):
             package_id="dummy_package_id",
             s3_path="dummy_s3_path",
             s3_bucket_name="dummy_s3_bucket_name",
+            destination_path=None,
             admin_metadata={},
+            status=IngestStatus.pending_transfer_to_dropbox,
             depositing_application=DepositingApplication.Dataverse
         )
 
