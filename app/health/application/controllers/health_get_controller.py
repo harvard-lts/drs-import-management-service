2e6f85d1794995bcb54bb6641cd814e354c42907
GPortas <hey@gportas.me>
2022-05-04 14:40:46 +0100

    Stash: git service WIP for recovering version hash in health endpoint

--- app/health/application/controllers/health_get_controller.py
+++ app/health/application/controllers/health_get_controller.py
@@ -1,7 +1,9 @@
 from typing import Tuple
 
+from app.ingest.application.controllers.services.git_service import GitService
+
 
 class HealthGetController:
 
     def __call__(self) -> Tuple[str, int]:
-        return "OK!", 200
+        return "OK!" + " " + GitService().get_commit_hash(), 200
---
+++ app/ingest/application/controllers/services/git_service.py
@@ -0,0 +1,10 @@
+from git import Repo
+
+
+class GitService:
+    __PATH_TO_REPO = "/home/dimsuser"
+
+    def get_commit_hash(self) -> str:
+        repo = Repo(self.__PATH_TO_REPO)
+        commit_hash = repo.git.rev_parse("HEAD")
+        return commit_hash
