1e3ee1a9673943cb13faa12127c64fce4eb42641
GPortas <hey@gportas.me>
2022-05-06 15:53:06 +0100

    Added: connectivity service for Dataverse

--- app/health/infrastructure/dataverse/services/dataverse_connectivity_service.py
+++ app/health/infrastructure/dataverse/services/dataverse_connectivity_service.py
@@ -1,14 +1,31 @@
+import logging
+import os
 from typing import Tuple
 
 from healthcheck import HealthCheck
+from requests import get, exceptions, HTTPError
 
 from app.health.infrastructure.connectivity_service import ConnectivityService
 
 
 class DataverseConnectivityService(ConnectivityService):
+    __DATAVERSE_CONN_OK_MESSAGE = "Dataverse connection OK"
+    __DATAVERSE_CONN_KO_MESSAGE = "Dataverse connection KO"
 
     def create_connectivity_check(self, health_check: HealthCheck) -> None:
         health_check.add_check(self.check_dataverse_connection)
 
     def check_dataverse_connection(self) -> Tuple[bool, str]:
-        return True, "Dataverse connection OK"
+        logger = logging.getLogger()
+        logger.info("Checking Dataverse connectivity...")
+        try:
+            dataverse_base_url = os.getenv('DATAVERSE_BASE_URL')
+            logger.debug("Dataverse base url: " + dataverse_base_url)
+            logger.debug("Executing GET operation...")
+            get(url=f"{dataverse_base_url}")
+            logger.info(self.__DATAVERSE_CONN_OK_MESSAGE)
+            return True, self.__DATAVERSE_CONN_OK_MESSAGE
+        except (exceptions.ConnectionError, HTTPError) as e:
+            logger.error(str(e))
+            logger.info(self.__DATAVERSE_CONN_KO_MESSAGE)
+            return False, self.__DATAVERSE_CONN_KO_MESSAGE
