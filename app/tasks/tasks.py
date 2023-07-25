fb559068a47d84fb97e1dd8f0767845ba917b16f
vac765 <valdeva_crema@harvard.edu>
2023-07-25 09:42:52 -0400

    auto retries

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -14,21 +14,34 @@ process_task = os.getenv('PROCESS_TASK_NAME', 'dims.tasks.handle_process_status'
 transfer_task = os.getenv('TRANSFER_TASK_NAME', 'dims.tasks.handle_transfer_status')
 retries = os.getenv('MESSAGE_MAX_RETRIES', 3)
 
-@app.task(bind=True, serializer='json', name=process_task, max_retries=retries, acks_late=True)
+@app.task(bind=True, serializer='json', name=process_task, max_retries=retries, acks_late=True, autoretry_for=(Exception,))
 def handle_process_status(self, message_body):
     if "dlq_testing" in message_body:
-        raise Reject("reject", requeue=False)
+        if self.request.retries < retries:
+            logger.debug("retrying DIMS Process")
+            self.retry(countdown=3)
+        else:
+            logger.debug("Sending to DLQ")
+            send_max_retry_notifications(message_body)
+            raise Reject("reject", requeue=False)  
+    
     try:
         process_service = Services.process_service()
         process_service.handle_process_status_message(message_body, self.request.id)
     except ProcessServiceException as e:
         exception_msg = traceback.format_exc()
-        send_error_notifications(message_body, e, exception_msg)
+        send_error_notifications(message_body, e, exception_msg, self.request.retries)
         
 @app.task(bind=True, serializer='json', name=transfer_task, max_retries=retries, acks_late=True)
 def handle_transfer_status(self, message_body):
     if "dlq_testing" in message_body:
-        raise Reject("reject", requeue=False)
+        if self.request.retries < retries:
+            logger.debug("retrying DIMS Transfer")
+            self.retry(countdown=3)
+        else:
+            logger.debug("Sending to DLQ")
+            send_max_retry_notifications(message_body)
+            raise Reject("reject", requeue=False) 
     try:
         transfer_service = Services.transfer_service()
         transfer_service.handle_transfer_status_message(message_body, self.request.id)
@@ -37,9 +50,18 @@ def handle_transfer_status(self, message_body):
                     queue=(os.getenv("PROCESS_PUBLISH_QUEUE_NAME") +'-dryrun')) 
     except TransferServiceException as e:
         exception_msg = traceback.format_exc()
-        send_error_notifications(message_body, e, exception_msg)
+        send_error_notifications(message_body, e, exception_msg, self.request.retries)
 
-def send_error_notifications(message_body, exception, exception_msg):
+def send_error_notifications(message_body, exception, exception_msg, num_retries):
     msg = "Could not process export for DRSIngest for {}.  Error {}.".format(message_body.get("package_id"), str(exception))
     body = msg + "\n" + exception_msg
     notifier.send_error_notification(str(exception), body)
+    #If too many retries happened
+    if num_retries == retries: 
+        send_max_retry_notifications(message_body)
+
+def send_max_retry_notifications(message_body):
+    subject = "Maximum resubmitting retries reached."
+    body = "Maximum resubmitting retries reached.\n\n" \
+        "The message has been consumed and will not be resubmitted again."
+    notifier.send_error_notification(subject, body)
