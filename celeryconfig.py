ad179bdbfdbb90f8ace0d05bb8f410a725b2c13d
vac765 <valdeva_crema@harvard.edu>
2023-08-09 10:27:51 -0400

    Removing unused code and commenting

--- celeryconfig.py
+++ celeryconfig.py
@@ -17,23 +17,27 @@ dead_letter_queue_option = {
     'x-message-ttl': int(os.getenv('MESSAGE_EXPIRATION_MS', 3600000))
 }
  
+# Set the default exchange to the same as the queue name
 default_process_exchange = Exchange(os.getenv("PROCESS_CONSUME_QUEUE_NAME"), type='direct')
+
+# Create the Queue and pass the DLQ options to it
+# This will route any failed messages to the DLQ
 default_process_queue = Queue(
     os.getenv("PROCESS_CONSUME_QUEUE_NAME"),
     default_process_exchange,
     routing_key=os.getenv("PROCESS_CONSUME_QUEUE_NAME"),
     queue_arguments=dead_letter_queue_option)
- 
+
+# Set the default exchange to the same as the queue name
 default_transfer_exchange = Exchange(os.getenv("TRANSFER_CONSUME_QUEUE_NAME"), type='direct')
+
+# Create the Queue and pass the DLQ options to it
+# This will route any failed messages to the DLQ
 default_transfer_queue = Queue(
     os.getenv("TRANSFER_CONSUME_QUEUE_NAME"),
     default_transfer_exchange,
     routing_key=os.getenv("TRANSFER_CONSUME_QUEUE_NAME"),
     queue_arguments=dead_letter_queue_option)
- 
-dlx_exchange = Exchange(os.getenv("DLQ_EXCHANGE_NAME"), type='direct')
-dead_letter_queue = Queue(
-    os.getenv("DLQ_QUEUE_NAME"), dlx_exchange, routing_key=os.getenv("DLQ_QUEUE_NAME"))
  
 task_queues = [default_process_queue, default_transfer_queue]
 
