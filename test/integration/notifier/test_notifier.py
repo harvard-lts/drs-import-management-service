602d7dec62d98e57aadb38a6efdf5d644efef630
Chip Goines <chip_goines@harvard.edu>
2023-09-08 10:13:25 -0400

    changed assert

--- test/integration/notifier/test_notifier.py
+++ test/integration/notifier/test_notifier.py
@@ -1,10 +1,10 @@
 import json, sys
+from celery.result import AsyncResult
 from test.integration.integration_test_base import IntegrationTestBase
 sys.path.append('app')
 import app.notifier.notifier as notifier 
 
 class TestNotifier(IntegrationTestBase):
     def test_notifier(self):
-        message = notifier.send_error_notification("Test Subject from DIMS", "Test Body from DIMS", "dts@hu.onmicrosoft.com")
-        json_message = json.loads(message)
-        self.assertTrue(type(json_message) is dict)
+        result = notifier.send_error_notification("Test Subject from DIMS", "Test Body from DIMS", "dts@hu.onmicrosoft.com")
+        assert isinstance(result, AsyncResult)
